b3a8772c42163b8b87feb780c2db63426ced88ea
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-26 12:04:39 +0530

    some ui changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -10,14 +10,14 @@ function TeamPage() {
         <div className="pt-30 flex justify-center ">
           <div className=" bg-gradient-to-br from-blue-500 to-red-500 rounded-4xl py-40 px-40 flex flex-col hover:bg-transparent transition delay-150 duration-100 ease-in-out hover:translate-y-1 hover:scale-110 ">
             <Image
-              src="/CuteCat.png"
+              src="/arch.gif"
               alt="Description"
               width={160}
               height={160}
-              className=" flex  w-40 h-40 rounded-full hover:cursor-pointer"
+              className=" flex  w-50 h-50 rounded-full hover:cursor-pointer "
             />
 
-            <p className="text-4xl font-bold text-center pt-4">Linux Guy</p>
+            <p className="text-4xl font-bold text-center pt-8">Linux Guy</p>
             <p className="text-center">
               Full Stack Developer <br /> Using Arch btw
             </p>
